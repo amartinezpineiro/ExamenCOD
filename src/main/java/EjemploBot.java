7d46ddb134a4cabc56aefbec2edb58a82e03225a
Adrian Martinez <amartinezpineiro@danielcastelao.org>
2022-03-16 14:02:41 +0100

    Comentado el codigo de la clase EjemploBot

--- src/main/java/EjemploBot.java
+++ src/main/java/EjemploBot.java
@@ -6,18 +6,18 @@ import discord4j.core.object.entity.channel.MessageChannel;
 
 public class EjemploBot {
     public static void main(final String[] args) {
-        final String token = args[0];
-        final DiscordClient client = DiscordClient.create(token);
-        final GatewayDiscordClient gateway = client.login().block();
+        final String token = "OTUzNjI4NTg3Njg3Mjg0NzM2.YjHV1A.SEwLO_D5dmRAGR6sfNKbe6qx1Fo";//Se crea una variable String para almacenar el token
+        final DiscordClient client = DiscordClient.create(token);//Se crea el cliente utilizado el token
+        final GatewayDiscordClient gateway = client.login().block();//Se crea una pasarela usando el cliente
 
         gateway.on(MessageCreateEvent.class).subscribe(event -> {
-            final Message message = event.getMessage();
-            if ("!ping".equals(message.getContent())) {
+            final Message message = event.getMessage();//Se crea una variable Message para recivir el mensage recivido
+            if ("!ping".equals(message.getContent())) {//Comprobacion de si el mensaje recivido equivale a !ping
                 final MessageChannel channel = message.getChannel().block();
-                channel.createMessage("Pong!").block();
+                channel.createMessage("Pong!").block();//Se crea el mensaje de respuesta Pong!
             }
         });
 
-        gateway.onDisconnect().block();
+        gateway.onDisconnect().block();//Se cierra la pasarela
     }
 }
